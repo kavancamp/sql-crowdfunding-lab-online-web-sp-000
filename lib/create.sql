de28567d1eb3d1ccf24d0af495a08072b52279cd
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-19 18:48:11 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,8 +5,7 @@ CREATE TABLE crowdfunder (
     funding_goal INTEGER,
     start_date INTEGER,
     end_date INTEGER
-
-);
+  );
 
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
@@ -15,5 +14,10 @@ CREATE TABLE projects (
     funding_goal INTEGER,
     start_date INTEGER,
     end_date INTEGER
+  );
 
-);
+CREATE TABLE users(
+ id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+  );
